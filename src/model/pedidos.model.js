93f80149c354b3efc1e94d1a11e5582263dacebf
jhoaking <peredoroj@gmail.com>
2025-03-26 16:28:47 -0400

    modelo creado de los pedidos conectada a la DB

---
+++ src/model/pedidos.model.js
@@ -0,0 +1,62 @@
+import {connection} from '../db.js';
+
+
+export class pedidoModel{
+    static obtenerPedido = async () =>{
+        try {
+            const [result] = await connection.query
+            ('SELECT BIN_TO_UUID(pedido_id) AS pedido_id, estado_id,frcha_pedido FROM pedidos')
+
+            return result;
+        } catch (error) {
+            console.error("Error al obtener los datos de la base de datos:", error);
+            throw error
+        }
+    }
+
+    static crearPedidos = async ({estado_id,usuario_id,fecha_pedido}) =>{
+        try {
+            const [rows] = await connection.query
+            ('INSERT INTO pedidos(estado_id,usuario_id,fecha_pedido) VALUES(?,UUID_TO_BIN(?),?)'[estado_id,usuario_id,fecha_pedido])
+
+            return rows;
+        } catch (error) {
+            console.error("Error al crear los datos de la base de datos:", error);
+            throw error
+        }
+    }
+
+    static eliminarPedidos = async (id) =>{
+        try {
+            const [result] = await connection.query('DELETE FROM pedidos WHERE pedido_id = UUID_TO_BIN(?)',[id]);
+
+            if(result.affectedRows === 0){
+                throw new Error ('no se elimino el pedido de la DB');
+            }
+
+            return result[0];
+        } catch (error) {
+            console.error("Error al crear los datos de la base de datos:", error);
+            throw error
+        }
+    }
+
+    static actuaizarPedido = async ({estado_id,fecha_pedido},id) =>{
+        try {
+            const [rows] = await connection.query
+            ('UPDATE pedidos SET estado_id = ? , fecha_pedido = ? WHERE pedido_id = UUID_TO_BIN(?)',[estado_id,fecha_pedido]);
+
+            if(rows.affectedRows === 0){
+                throw new Error ('no se actualizo el pedido de la DB');
+            }
+
+            const [result] = await connection.query
+            ('SELECT BIN_TO_UUID(pedido_id) AS pedido_id, estado_id,fecha_pedido FROM pedidos WHERE pedido_id = UUID_TO_BIN(?)',[id]);
+
+            return result[0];
+        } catch (error) {
+            console.error("Error al actualizar los datos de la base de datos:", error);
+            throw error
+        }
+    }
+}
---
+++ src/schema/validacionPedidos.js
@@ -0,0 +1,11 @@
+import z from 'zod';
+
+const pedidosSchema = z.object({
+    estado_id: z.number().int().positive(),
+    usuario_id : z.string().uuid(),
+    fecha_pedido: z.string().regex(/^\d{4}-\d{2}-\d{2}$/)
+})
+
+export const validarPedido = (input) =>{
+    return pedidosSchema.partial().safeParse();
+}
